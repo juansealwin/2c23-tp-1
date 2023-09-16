45a65023accf3ec7da4eae906841df513ec4aa4b
Julián <juliangorge@hotmail.com>
2023-09-16 18:17:43 -0300

    Update index.js

--- app/index.js
+++ app/index.js
@@ -6,6 +6,22 @@ app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
+app.get('/ping', (req, res) => {
+    res.send('Pong!')
+})
+
+app.get('/metar', (req, res) => {
+    res.send('Metar')
+})
+
+app.get('/spaceflight_news', (req, res) => {
+    res.send('Spaceflight news')
+})
+
+app.get('/quote', (req, res) => {
+    res.send('Quote')
+})
+
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
