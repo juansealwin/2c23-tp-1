7171b20b702e2d6c4c97ee199eedfdea12f93c15
Julián <juliangorge@hotmail.com>
2023-09-21 20:36:05 -0300

    Endpoints spaceflight_news y quote

--- app/index.js
+++ app/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const axios = require('axios')
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
@@ -14,12 +15,29 @@ app.get('/metar', (req, res) => {
     res.send('Metar')
 })
 
-app.get('/spaceflight_news', (req, res) => {
-    res.send('Spaceflight news')
+app.get('/spaceflight_news', async (req, res) => {
+    try {
+        const response = await axios.get('https://api.spaceflightnewsapi.net/v3/articles?_limit=5');
+        const titles = response.data.map(item => item.title);
+        res.send(titles)
+    } catch (e) {
+        console.error(e);
+        res.status(500).send('Error al obtener datos');
+    }
 })
 
-app.get('/quote', (req, res) => {
-    res.send('Quote')
+app.get('/quote', async (req, res) => {
+    try {
+        const response = await axios.get('https://api.quotable.io/random');
+
+        res.send({
+            'content': response.data.content,
+            'author': response.data.author
+        })
+    } catch (e) {
+        console.error(e);
+        res.status(500).send('Error al obtener datos');
+    }
 })
 
 app.listen(port, () => {
