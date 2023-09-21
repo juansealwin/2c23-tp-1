885a56284277f9046ce7c65900f61a7a4040e9d3
Felipe Marelli <felipemarelli@remind101.com>
2023-09-21 20:45:44 -0300

    Endpoint Metar

--- app/index.js
+++ app/index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const app = express()
 const port = 3000
-const axios = require('axios')
+const axios = require('axios');
+const {XMLParser} = require('fast-xml-parser');
+const {decode} = require('metar-decoder');
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
@@ -11,8 +13,23 @@ app.get('/ping', (req, res) => {
     res.send('Pong!')
 })
 
-app.get('/metar', (req, res) => {
-    res.send('Metar')
+app.get('/metar', async (req, res) => {
+    try {
+        const parser = new XMLParser();
+        const stationCode = req.query.station;
+        if(!stationCode)
+            return res.status(400).send("Especifique el aeródromo con ?station={code}");
+
+        const response = await axios.get(`https://www.aviationweather.gov/adds/dataserver_current/httpparam?dataSource=metars&requestType=retrieve&format=xml&stationString=${stationCode}&hoursBeforeNow=1`);
+        //Convertimos el XML obtenido a JSON, por conveniencia
+        const parsed = parser.parse(response.data);
+        //Decodificamos el METAR
+        const decodedMetar = decode(parsed.response.data.METAR.raw_text);
+
+        res.send(decodedMetar)
+    } catch (error) {
+        res.status(500).send('Error al obtener datos');
+    }
 })
 
 app.get('/spaceflight_news', async (req, res) => {
